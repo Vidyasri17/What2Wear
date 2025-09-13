4a4cf30dd71cad9144c4642cb1f1ce54d32c3c5b
Vidyasri17 <147472710+Vidyasri17@users.noreply.github.com>
2025-09-13 20:10:57 +0530

    Delete What2Wear/Weather based Outfit predictor directory

--- What2Wear/Weather based Outfit predictor/config.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-API_KEY = os.getenv("OPENWEATHER_API_KEY")  # Put your key in .env
-BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
-
